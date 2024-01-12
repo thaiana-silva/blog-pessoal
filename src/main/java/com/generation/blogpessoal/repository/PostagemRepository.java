f9aae12ff8b1243b1506395e0c37e644e44a6e6b
Thaiana Silva <thaianacelia@gmail.com>
2024-01-12 12:22:02 -0300

    adicionando arquivos do pacote postagem

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,16 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
-//JPA dá poder a repository para comunicar-se com o banco de dados
-public interface PostagemRepository extends JpaRepository <Postagem, Long> {
 
+
+//JPA dá poder a repository para comunicar-se com o banco de dados
+@Repository
+public interface PostagemRepository extends JpaRepository <Postagem, Long> {
+	List<Postagem> findAllByTittuloContainIgnoreCase(@Param("titulo") String titulo);
 }
